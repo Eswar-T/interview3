bee532f9ce684be85834299b16ec88140071e43c
Eswar-T <t.eswarvarun@gmail.com>
2020-08-06 05:05:23 -0700

    code added

--- src/Quotes.js
+++ src/Quotes.js
@@ -28,21 +28,31 @@ class Quotes extends Component {
   }
 
   componentDidMount =()=>{
-           fetch("http://opinionated-quotes-api.gigalixirapp.com/v1/quotes?n=10")
+           fetch("http://opinionated-quotes-api.gigalixirapp.com/v1/quotes?n=50")
            .then(Response=>Response.json())
            .then(response=> this.setState({ dataSource: response.quotes },()=>console.log(response.quotes,"response")) )
            .catch(error=> console.log("error",error))
+  }
+
+  empty = () => {
+     console.warn("no more data")
   }
   render(){
   return (
    <View style={{ justifyContent:'center',alignItems:'center' }}>
        <FlatList  data={this.state.dataSource}
                   showsVerticalScrollIndicator={false}
-                  renderItem={({item})=> 
-                                   <View style={{ height:'auto',width:'94%', marginTop:16,backgroundColor:'#87CEEB',borderRadius:6,marginLeft:10,marginRight:10,padding:6 }}>
+                  initialNumToRender={50}
+                  renderItem={({item,index})=> 
+                                  
+                                   <View style={{ height:'auto',width:'94%',backgroundColor:'#87CEEB',borderRadius:6,marginLeft:10,marginRight:10,padding:10,marginTop:12,marginBottom:10 }}>
                                      <Text>{item.quote}</Text>
-                                     </View>}
-                  keyExtractor={(item,index) => index.toString()}                   />
+                                     </View>
+                }
+                  keyExtractor={(item,index) => index.toString()} 
+                  onEndReached={this.empty} 
+                    
+                               />
     </View>
   );
   }
